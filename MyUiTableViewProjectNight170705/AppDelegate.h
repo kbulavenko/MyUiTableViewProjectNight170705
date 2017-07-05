0fd19af917e8af2e4520a1fa0f712ef399a04e41
Z <z@imac-z.local>
2017-07-05 23:54:52 +0300

    Initial Commit

---
+++ MyUiTableViewProjectNight170705/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyUiTableViewProjectNight170705
+//
+//  Created by Z on 05.07.17.
+//  Copyright © 2017 Z. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyUiTableViewProjectNight170705/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyUiTableViewProjectNight170705
+//
+//  Created by Z on 05.07.17.
+//  Copyright © 2017 Z. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
